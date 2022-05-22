96cdb6e1496eb01b5d19571a06aff71e771a1e92
TejaswiniSruthi <82491774+TejaswiniSruthi@users.noreply.github.com>
2022-05-22 13:46:24 +0530

    Create Even_numbers_between_even_nums_arr.cpp

---
+++ Even_numbers_between_even_nums_arr.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,count=0,m,o;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            m=i;
+            break;
+        }
+    }
+    for(int i=n-1;i>=m;i--){
+        if(arr[i]%2==0){
+            o=i;
+            break;
+        }
+    }
+    for(int i=m+1;i<o;i++){
+        if(arr[i]%2==0){
+            count++;
+        }
+        else{
+            continue;
+        }
+    }
+    cout<<count;
+}
